d670e8dba510d90d4e3583f955189f13670171d4
G1998G <60283066+G1998G@users.noreply.github.com>
2022-08-24 16:24:44 +0900

    絵文字の処理を修正

--- wordcloud_pros.py
+++ wordcloud_pros.py
@@ -264,20 +264,27 @@ class MakeWordCloud:
             draw = ImageDraw.Draw(self.base_img)
             #重複する絵文字を集合にする
             emoji_set = set(self.emojilist)
+            #絵文字抜きのw_count_dictを作成する
+            w_count_dict_without_emoji = self.w_count_dict
+            
            
-            for emoji in emoji_set:
+            for key,value in self.w_count_dict.items():
                 # 絵文字サイズを絵文字出現回数で決める
-                # 160をサイズとする
-                size = 100
-                left_x,left_y = random.randint(0,800-size),random.randint(0,500-size)
-                right_x,right_y = left_x+min(c*60,size),left_y+min(c*60,size)
-                # [左上のx座標, 左上のy座標, 右下のx座標, 右下のy座標]
-                xylist = [left_x,left_y,right_x,right_y]
-                # 絵文字スペースを白色で描写
-                draw.rectangle(xylist, fill=(255,255,255))
-                # emoji_xy辞書に情報追加
-                url = self.emojidict[emoji]
-                self.emoji_xy[url] = xylist
+                if value in emoji_set:
+                    # 絵文字サイズ
+                    size = 100*( 1+(key*0.01) )
+                    left_x,left_y = random.randint(0,800-size),random.randint(0,500-size)
+                    right_x,right_y = left_x+min(c*60,size),left_y+min(c*60,size)
+                    # [左上のx座標, 左上のy座標, 右下のx座標, 右下のy座標]
+                    xylist = [left_x,left_y,right_x,right_y]
+                    # 絵文字スペースを白色で描写
+                    draw.rectangle(xylist, fill=(255,255,255))
+                    # emoji_xy辞書に情報追加
+                    url = self.emojidict[emoji]
+                    self.emoji_xy[url] = xylist
+                    #w_count_dict_without_emojiから絵文字を削除
+                    del w_count_dict_without_emoj[key]
+                    
             if self.w_count_dict:
                 print('絵文字ありワードあり')
                 return self.wordcloud()
